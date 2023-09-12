67f4c84208731e949ee6b2e095ba8f1f2e9c760c
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-09-12 19:26:36 +0530

    UnderStand Reason Why Node.Js is So Popular

---
+++ 00-Getting_Started_With_Node.Js/01-Why_Node_Js_is_Popular.js
@@ -0,0 +1,26 @@
+/*
+    In the 2022 Stack OverFlow Conduct a Survey to Know Which Tool or Programming Language is 
+    Most Popular Among the Developers and the Survey Result is :
+
+    1. Node.Js              47.12% Developers
+    2. React.Js             42.62% Developers
+    3. jQuery               28.57% Developers
+    4. Express              22.99% Developers
+*/
+
+/*
+    Application Which is Builts On Node.JS :
+    1. Linkedin
+    2. NetFlix
+    3. Paypal
+    4. Uber
+
+*/
+
+/*
+    Why Node.Js is So Popular :
+    1. High Performance ( Ability To Handle Multiple Request at a Time )
+    2. Role of JavaScript ( JavaScript is top on Stack Overflow Servey in 2022 among all Scripts, programming and Markup Languages with 66.38% Developers)
+    3. LightWeight ( Simple to Run On Almost Every System )
+    4. Works Well with Data Intensive Applications (Like.. Facebook, Linkedin)
+*/
