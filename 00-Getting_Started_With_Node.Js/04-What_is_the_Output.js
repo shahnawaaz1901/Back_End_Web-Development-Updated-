7b2356d4bab3c0533651f8c5e6371df6a89c9040
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-09-12 20:05:31 +0530

    MCQs Around Node.Js

---
+++ 00-Getting_Started_With_Node.Js/04-What_is_the_Output.js
@@ -0,0 +1,17 @@
+/*
+Runtimes
+
+Which of the following role(s) does Node.js play as a runtime? (Multiple)
+
+1. Memory management                                // Correct
+2. Handling input/output operations                 // Correct
+3. Garbage collection                               // Correct
+4. Database management                              
+
+Solution Description : Node.js does not play a role in database management directly. 
+                While it's possible to interact with databases (like MySQL, MongoDB, etc.) 
+                using Node.js via appropriate packages, Node.js itself is not responsible 
+                for the management of these databases. That is handled by the specific 
+                database software itself.
+
+*/
