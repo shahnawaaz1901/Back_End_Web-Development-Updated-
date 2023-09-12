35956c993be2bb620ecea7bfa54bd77ef6696387
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-09-12 19:05:27 +0530

    Introduction to Node.JS

---
+++ 00-Gettin_Started_With_Node.Js/00-Introduction_to_NodeJs.js
@@ -0,0 +1,48 @@
+/*
+    Node.Js : Node.Js is Not Programming Language framework library, Node.Js is An 
+            Open Source Cross Platform ServerSide Runtime for JavaScript.
+
+            Node.Js is Server side tool We Not Use Node.Js for FrontEnd.
+
+            Every Programming Has Runtime to run To use the Language JavaScript Also
+            has it's RunTime, For FrontEnd Browser Acts Like Runtime for JavaScript 
+            Which Helps you to create front end application.
+
+            Like FrontEnd JavaScript Has RunTime to Run On Server Side that's Node.Js
+
+            For JavaScript You Can Use Browser as Runtime for Front End or Client Side 
+            and Node.Js as Runtime for Server Side.
+    
+    OpenSource : Open Source Means which is Available to EveryOne Which Code is 
+                Available for All
+*/
+
+
+/*
+    Why Node.Js :
+    There Are Many Technologies to handle Server Side , But Why Node.Js is Popular Because
+    Node.Js Give Something Extra :
+    1. Handles Asynchronous Operations Better
+    2. Faster Then Existing Tools in Many Use Cases
+
+*/
+
+/*
+    Founder Of Node.Js
+    Name : Ryan Dahi
+    Year : 2009
+*/
+
+/*
+    How Node.Js is Created :
+    We Learn that Browser Like Google Chrome and Node.Js is Runtime for JavaScript 
+    for Client Side and Sever Side Respectively.
+    Google Chrome Uses JavaScript Engine V8, Which is Open Source.
+    As We Know Google Chrome is The Fastest Browser Because is Uses V8 Engine of 
+    JavaScript. V8 Engine Very Well Optimized to Run JavaScript on Front End.
+
+    Ryan Dahi Some Change in V8 Engine using C++ and Create a Runtime for JavaScript 
+    on Server Side which is Node.Js.
+    C++ not Prequist to Learn Node.Js
+
+*/
