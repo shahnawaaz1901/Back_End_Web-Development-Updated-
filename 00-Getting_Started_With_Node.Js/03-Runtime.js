75bd42a68aa653cb532796f12bd86b2a99b23dc9
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-09-12 19:53:42 +0530

    UnderStand Runtime Environment

---
+++ 00-Getting_Started_With_Node.Js/03-Runtime.js
@@ -0,0 +1,34 @@
+/*
+    Runtime : Runtime is The Environment or Platform Which is Basically helps to
+            Executes Any Programming Language or You Can Say Job of Runtime is 
+            Executes the Programming Language
+
+    What Runtime Do :
+    1. Compilers And Interprets : If Language Needs Compilation then Runtime is Responsible 
+        And if Language needs interpretation then it Runtime Interpret Code.
+    2. Memory Management : Runtime is Responsible to Store Variable, Function and Other things
+        which Requires Memory in our System.
+    3. Handles I/O Operations : I/O Operations is Operation Are those Operations which Needs 
+        to Communicate with External Systems Like Databases API and Others.
+    4. Guarbage Collection : Guarbage Collection is Used to DeAllocated Memory Which is 
+        Allocated For functions which are not Used.
+
+*/
+
+
+/*
+    Runtime For Programming Language :
+    1. Java :- Java Virtual Machine(JVM)
+    2. Python :- Cpython
+*/
+
+/*
+    List of Engines Which is Used by the Browser
+    1. Google Chrome : V8 Engine
+    2. Microsoft Edge : Chakra (Later Updated in V8 Engine 2020
+    3. Apple Safari :  JavaScript Core
+    4. Mozilla Firefox : SpiderMonkey
+
+
+    V8 is the Fastest Engine Among All of them So that V8 Engine is Used to Built Node.Js
+*/
