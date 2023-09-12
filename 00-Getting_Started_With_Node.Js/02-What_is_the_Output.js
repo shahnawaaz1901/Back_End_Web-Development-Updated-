060acc7bb876a5a3d9e65d893e98c1506c26d073
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-09-12 19:30:34 +0530

    MCQs Around Node.Js

---
+++ 00-Getting_Started_With_Node.Js/02-What_is_the_Output.js
@@ -0,0 +1,17 @@
+/*
+Popularity of Node.js
+
+What are the contributing factors to the popularity of Node.js? ( Multiple )
+
+1. Node.js utilizes JavaScript, a language familiar to many developers.                 // Correct
+2. Node.js is optimized for scalability and high performance.                           // Correct
+3. Node.js implements an event-driven, non-blocking I/O model, enabling                 // Correct
+    efficient and speedy data handling.
+4. Node.js implements a synchronous I/O model that offers predictable and reliable data handling.
+
+Solution Description : Option 4 is incorrect as node.js utilizes an event-driven, 
+                non-blocking I/O model, which allows for concurrent handling of multiple 
+                requests without blocking the execution flow.
+
+
+*/
