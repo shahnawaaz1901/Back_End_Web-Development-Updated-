80fe73e7371baa3eaa8fbe6e07e590a885a8fbdf
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-09-14 19:32:04 +0530

    UnderStand Servers

---
+++ 00-Getting_Started_With_Node.Js/10-Server.js
@@ -0,0 +1,32 @@
+/*
+    Server : As Name Specify Server Serves to the Client.
+
+    We Know that every webApplication have 2 parts one is CLient side and Other is Server Side.
+    Client Sends the Request to the server and server send response for that perticular request
+*/
+
+/*
+    Responsibilities of Server : 
+    1. Handling Request which receive from the Client and sending response to the client
+    2. Authentication 
+    3. Database Accesses
+    4. File System Accesses
+
+*/
+
+/*
+Servers
+
+Which of the following statements is/are correct about servers? ( Multiple )
+
+1. Servers are responsible for handling user authentication and                 // Correct
+    processing client requests.
+2. Servers can deliver and process data.                                        // Correct
+3. Servers are located on the server side of a web application.                 // Correct
+4. Servers can be either physical machines or virtual machines.                 // Correct
+
+Solution Description : Servers are responsible for managing user authentication, 
+            handling client requests, and querying databases. They can deliver 
+            data and perform processing tasks, are located on the server side of 
+            a web application, and can exist as physical or virtual machines. 
+*/
