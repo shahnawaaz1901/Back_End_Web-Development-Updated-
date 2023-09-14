e8506b8bf2d86de32f37b5c30dacd0bb79e088d3
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-09-14 19:43:17 +0530

    UnderStand HTTP

---
+++ 00-Getting_Started_With_Node.Js/11-HTTP_Server.js
@@ -0,0 +1,10 @@
+/*
+    HTTP : For Communicate or Sharing Data between client to server Both Server and Client
+    Follows some set of Rules in Order to Communicate which is (HTTP)Hyper Text Transfer Protocol.
+*/
+
+/*
+    If Client Not Follows Complete Rules of HTTP and sends Request to the server then
+    server faces some issues or some kind of problems to understand and send response
+*/
+
