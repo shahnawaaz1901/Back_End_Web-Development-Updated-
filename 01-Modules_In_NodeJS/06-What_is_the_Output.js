bd9f6c92a06beda3b9fde6c5d6fba438b4bf0353
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-09-16 21:13:17 +0530

    MCQs Around Modules

---
+++ 01-Modules_In_NodeJS/06-What_is_the_Output.js
@@ -0,0 +1,34 @@
+/*
+Local Modules
+
+Which statement(s) is/are true regarding local modules in Node.js? ( Multiple )
+
+1. Local modules are custom modules created for our own application.            // Correct
+2. Local modules can be used to organize our code and create reusable           // Correct
+    components.
+3. Local modules are built-in modules that come with Node.js.           
+4. Local modules are created by other developers and published on package 
+    registries like npm.
+
+Solution Description : Local modules in Node.js are custom modules created by 
+                developers for their own application's specific needs. They can 
+                be used to organize code and create reusable components. However, 
+                they are not built-in modules that come with Node.js, and they are 
+                not created by other developers and published on package registries 
+                like npm.
+*/
+
+/*
+Core Modules
+
+Which of the following statement is true about core modules in Node.js?
+
+1. Core modules are built-in modules in Node.js.                                // Correct
+2. Core modules are third-party modules installed using npm.
+3. Core modules are modules written by the user for specific use cases.
+4. Core modules are only used for testing purposes in Node.js.
+
+Solution Description : Core modules are modules that come pre-installed with 
+                Node.js and can be used by developers without requiring any 
+                additional installation or configuration. 
+*/
