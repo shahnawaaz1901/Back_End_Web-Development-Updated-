7b5bf75c5b54466bea0dc14134dab5f13d4049c5
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-09-16 21:08:31 +0530

    UnderStand Type of Modules

---
+++ 01-Modules_In_NodeJS/05-Types_of_Modules.js
@@ -0,0 +1,20 @@
+/*
+    Basically Modules Are Three Types :
+    1. Internal Modules
+    2. Core Modules
+    3. External Modules
+
+    Internal Modules/User Define Modules : Modules Which is Created by User and 
+        User Export module from a file and Import tha module file in Another File 
+        then this type of Modules is Called Internal Modules. This also Called User
+        Define Modules Because User is Created and exported this module
+
+    Core Modules : Modules Which is Written By NodeJS Team and we Directly Import
+        that module or we can say modules which comes from NodeJS and we Can Use 
+        those modules by just Importing them it's Called Core Modules
+    
+    External Modules : Modules which is Not Written By Us and Also Not Written by NodeJS
+        team, then where from this module comes, this modules comes from other company 
+        or third party to use that company software or file in NodeJs this type of Modules
+        is called External Modules
+*/
