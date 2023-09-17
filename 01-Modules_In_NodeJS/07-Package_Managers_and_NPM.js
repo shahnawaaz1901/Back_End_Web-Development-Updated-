370258dd4e864a6e0c6594e862294737e33d5d3e
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-09-17 18:50:41 +0530

    UnderStand Package Managers & NPM

---
+++ 01-Modules_In_NodeJS/07-Package_Managers_and_NPM.js
@@ -0,0 +1,50 @@
+/*
+    For Import and Use Thrid Party or External Modules We Have Some Other Tools to
+    Make this Happen For US, this tool is Known as Package Manager.
+
+    Package Manager : Management of Package is Known as Package Manager.
+    
+    Packages : Packages is term which Means All the Third Party or External Modules
+    which You work on Your Code .
+    
+    For Use Packages in Your File First You Need to Install package and after that 
+    you can use in your code.
+
+*/
+
+/*
+    Roles of Package Manager :
+    1. Helps Us to Install the Packages.
+    2. Manage the Version of Packages.( Means Write Code for the Package according to Specific Version )
+    3. Manage the Depenedecies
+    4. Publish New Packages ( Helps us to Publish the Package which Other Can Use )
+
+*/
+
+/*
+    Default Package Manager for Node.Js is NPM (Unofficial Name Node Package Manager)
+    NPM not Only Manage NodeJS Packages but also manages angular and react framework
+    packages so you can't say that NPM is Specifically Built for Node.JS
+
+    Other Package Manager is YARN.
+
+    Why NPM is default package Manager Because NPM is Automatically Install when You
+    Install NodeJS on Your System.
+
+    For Verify NPM install or Not you can check by npm -v
+*/
+
+/*
+    Mulptiple Programming Languages has their Package Manager
+    1. Java : Maven
+    2. .net : NuGet
+    3. Python : PIP
+
+    Every Programming Language will have their external dependencies Like NodeJS.
+    NPM is work on more then 1 million packages.
+    
+    For Install Packages in NodeJS You Can Directly Go to npm Web Page and look at that
+
+    For Install Package locally You Can Write Command :
+    npm i package_name
+*/
