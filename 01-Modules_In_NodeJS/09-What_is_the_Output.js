463c1b20683f9d39c831ff4f2c4bd6eb8e35aef6
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-09-18 18:52:28 +0530

    MCQ Around NPM

---
+++ 01-Modules_In_NodeJS/09-What_is_the_Output.js
@@ -0,0 +1,16 @@
+/*
+NPM
+
+Which of the following tasks can be performed by a package manager? ( Mulptiple )
+
+1. Managing dependencies between packages                       // Correct
+2. Removing packages                                            // Correct
+3. Resolve conflicts between package versions                   // Correct
+4. Hurray! Correct Answer                                       // Correct
+
+Solution Description : A package manager can perform all of the above tasks: 
+                installing and updating packages, 
+                managing dependencies between packages, 
+                removing software packages, and 
+                resolving conflicts between package versions.
+*/
