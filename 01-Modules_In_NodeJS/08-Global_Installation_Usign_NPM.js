d551b6c649c0bee209890b3c676b9cdd341493a6
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-09-18 18:50:09 +0530

    Learn Global Installation of Packages

---
+++ 01-Modules_In_NodeJS/08-Global_Installation_Usign_NPM.js
@@ -0,0 +1,15 @@
+/*
+    Locally Installation of npm packages is only valid in folder and VS Code, not 
+    in other folder and other IDE and Folder Can't Understand that Package.
+
+    So understood all IDE and All Folder You Need to Install Package Globally so 
+    that every folder and IDE understood of this package.
+
+    For install Package Globally You Need to Write Command :
+    npm i -g package_nam
+
+    where -g defines the package will install globally
+
+    When You Install Package Globally then package not found in package.json on 
+    dependencies.
+*/
