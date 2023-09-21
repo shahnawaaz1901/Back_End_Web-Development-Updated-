519ea6d5f59bcc2e2e6d1705f9073d37e834a24b
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-09-21 22:09:02 +0530

    Learn Debugging in VS Code

---
+++ 02-More_Modules/02-Debugging_in_VS_code.js
@@ -0,0 +1,37 @@
+function grandTotal(productsList){
+    let total = 0;
+    productsList.forEach((product) => {
+        total += product.price * product.quantity; 
+    });
+    return total;
+}
+
+function printTotal(total){
+    console.log(total);
+}
+let productsList = [
+    {
+        name : 'Hat',
+        price : 25,
+        quantity : 2
+    },
+    {
+        name : 'Shoe',
+        price : 50,
+        quantity : 3
+    },
+    {
+        name : 'Gloves',
+        price : 30,
+        quantity : 2
+    }
+
+]
+let total = grandTotal(productsList);
+printTotal(total);
+
+/*
+    For Debugging in VS Code We Need to First Create launch.json file which
+    contains information regarding our file.
+*/
+
