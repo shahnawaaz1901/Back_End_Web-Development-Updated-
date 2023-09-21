ad797bcf357031167a8a58c826b7257327bca268
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-09-21 20:36:43 +0530

    UnderStand How NodeJs Debugging is Different

---
+++ 02-More_Modules/01-How_Debugginfg_in_NodeJS_is_Different.js
@@ -0,0 +1,10 @@
+/*
+    How Node.Js Debugging is Different then in JavaScript :
+    1. Environment : Node.Js runs a sever side enviroment, whereas typically
+                javascript runs in the browser.
+    2. Debugging Tools : Node.Js has its Own BuiltIn Debugger, while JavaScript
+                relies on Browser Based Tools like developer tools in Chrome.
+    3. Access to Server Side Resource : When Accessing Debugging in NodeJs You Have
+                to Access to server side resources like the file system, databases 
+                and network connections
+*/
