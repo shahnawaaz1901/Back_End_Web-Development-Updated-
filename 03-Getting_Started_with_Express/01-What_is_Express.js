25dabb9facb0dae0d72846202008376b41760cce
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-09-24 21:17:02 +0530

    Introduction to Express

---
+++ 03-Getting_Started_with_Express/01-What_is_Express.js
@@ -0,0 +1,65 @@
+/*
+    In this Module We will See a Node.Js FrameWork which is Express
+*/
+
+/*
+    FrameWork : A Framework is helps Developers to Create Application in Less Time, Because
+            a framework has lot of inbuilt features, inbuilt libraries which developer can
+            use directly and make Application in Less time with More Efficiently
+*/
+
+/*
+    Almost Every Programming Language have their Framework.
+    Some FrameWork of Popular Programming Language :
+
+    Language                    FrameWork
+    1. Python                   Django
+    2. Java                     Springboot
+    3. C-Sharp                  .net
+
+    Like Other Programming Language Node.Js has Express framework which Allows User to Create 
+    Application in Less time
+*/
+
+/*
+    Why Express Comes into The Picture, Because Express Solve Many Problems which was faced by 
+    Many Developers While Creating an Application using NodeJS. 
+    
+    Some Major Problems Which Express Solves : 
+    1. Routing : Express Gives us inbuilt functions or you can say modules which helps us 
+            to manage the different url or requests. and In some Request a subRequest URL
+            is Required by the Client Which can easily Manage through Routing in Express
+    2. MiddleWare : When Ever User send Request to the server and server send that request 
+            to the request handler in between we can modify our request using middleware.
+            middleware have access to read or write request which server sent to request
+            handler.
+    3. Template Engine : When Ever User Sends request to the server and server gives the html
+            content to the user, that html content is static and same for all user instead of
+            sending same response and make static webpage we can send different responses to
+            the different users using template engine and make our web page dynamic.
+            Multiple Template Engines is Exist.
+*/
+
+/*
+    Express.Js is framework which is built on Node.JS for Understand express we need to 
+    understand Node.Js first and for Node.Js we Need to understand javascript because files
+    are stores in javascript format.
+
+*/
+
+/*
+Introduction to Express
+
+Which statement(s) is/are true regarding Express? ( Multiple )
+
+1. Express simplifies the process of creating server-side applications by handling          // Correct
+    much of the boilerplate and repetitive tasks.
+2. Express is a web framework for Node.js.                                                  // Correct
+3. Express provides a lightweight layer of essential features on top of Node.js.            // Correct
+4. Express is a complete replacement for Node.js.
+
+Solution Description : Express is a web application framework built on top of Node.js. While Express
+                    simplifies the process of building web applications with Node.js, it cannot be 
+                    considered a complete replacement for Node.js.
+
+*/
