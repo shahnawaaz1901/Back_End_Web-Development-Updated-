a6ab04c1076907c7202a811565807f0665fababf
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-09-24 20:38:41 +0530

    UnderStand Importance of Code Quality

---
+++ 02-More_Modules/11-Best_Practice.js
@@ -0,0 +1,58 @@
+/*
+    To Enhance Quality of Code We Need to remember some steps in mind :
+    
+    1. Modularize Your Code : When Ever You Write the code always write code into different modules 
+                    so that each module is independent and each module only handle a certain task
+                    for different task Create different Module because our code is more readable
+                    and we can easily able to debug our code and if one module creates a bug then
+                    because other module is independent other modules not affected
+    
+    2. Use Descriptive Naming Convensions : When Ever You Creates a Variable, functions or class 
+                    while making an application always gives name as per the working of that 
+                    function, variable and class.
+                    This Gives you and edge because if someone read your code you that person
+                    not need to checks the whole function if your function name gives an idea
+                    that hey this function do this task. So Naming Convension is Very Important
+                    and we Need to take this Seriously.
+        
+        <---
+            A developer write a Program Which Computer Understand But a Good Developer Write a Program
+            Which Human Can UnderStand
+        --->
+    
+    3. Keep Module Responsibilities Clear : This Means one Module perform only that certain task
+                    Means One Module Only Created for Doing One task if You Take More Responsibilities
+                    to one Module then Readability of Module is Not Good and if SomeOne Other can
+                    check your code that person stuck at the point of time and able to understand.
+
+        <---
+            If You Change in Module and more then 1 Module is Affected for changes in perticular module
+            then this Indicated the bad Practices because Other Module is Affected After Changes in a
+            perticular Module. 
+            For Changing in Module Only One Reason have to change the module more then One reason
+            also indicates bad Practices
+        --->
+    4. Use Version Control Like Git : Always Push Your Code into the Version Control System Like Git,
+                    Replit and other Version Control System. WithOut Version Controll System working 
+                    in a Team is A Night mare because how can other developer knows that what changes
+                    is done by whose.
+*/
+
+/*
+
+Best Practices
+
+Which of the following is/are best practice(s) for writing maintainable code in Node.js? ( Multiple )
+
+1. Clear and descriptive naming conventions for variables, functions, and modules       // Correct
+2. Minimal comments and no README files
+3. README file for each module, explaining functionality and usage                      // Correct
+4. Version control like Git for tracking code changes                                   // Correct
+
+Solution Description : To write maintainable code in Node.js, use clear and descriptive names 
+                    for variables, functions, and modules, create a README file for each module 
+                    to explain its usage, and use version control like Git to track code changes 
+                    and collaborate with other developers. Minimal comments and no README files are 
+                    not recommended as they make it harder for others to understand and maintain the code.
+
+*/
