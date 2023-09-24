d703472bf747b1de0b68ef1e6993f6ed8f093b78
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-09-24 19:50:23 +0530

    MCQ Around the Event Driven Architecture

---
+++ 02-More_Modules/10-What_is_the_Output.js
@@ -0,0 +1,17 @@
+/*
+Event-Driven Architecture
+
+How does Event-Driven Architecture improve the scalability of Node.js applications?
+
+1. By allowing the application to handle multiple tasks concurrently and        // Correct
+    asynchronously
+2. By reducing the number of tasks that the application can handle at a 
+    given time
+3. By decoupling the components of the application, making them more independent    // Correct
+    and scalable
+4. By making the application dependent on a single thread for all tasks
+
+Solution Description : Event-Driven Architecture improves scalability by allowing concurrent 
+                    and asynchronous task handling and promotes loose coupling between application 
+                    components, making them more independent and scalable to adapt to changing requirements.
+*/
