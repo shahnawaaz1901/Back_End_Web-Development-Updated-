3de688a9206cb3340d13529a0d12fe0213946f41
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-09-27 20:57:16 +0530

    UnderStand Most Popular Web Development Patterns

---
+++ 04-MVC_Part_I/00-Getting_Started_with_Web_Development.js
@@ -0,0 +1,58 @@
+/*
+    Web Development : Development of Web Application which a Web Application Which Usually 
+                Consists Client and Server. 
+*/
+
+/*  
+    Main Purpose of Express (A Web Development FrameWork) :
+    1. To Create Modern Web Applications            :- Modern Applications : Data Intensive Applications Like Facebook, Twitter or Instagram etc.
+    2. Create High Performance Web Applications     :- High Performance : Solve Multiple Queries in Less Time
+    3. Create Scalable Applications                 :- Scalable : Ability To Handle Large Users as per Requirements
+*/
+
+/*
+    For Creating a Web Applications We Also Need Understanding of Patterns Along With 
+    Framework to Create a Better Web Application. In Current Market Lots of Web Development
+    Pattern is Exist, But We Learn Most Three Most Popular Patterns :
+    1. MVC (Model View Controller) : MVC is One of The Most Popular Web Development Pattern in Market. 
+            MVC Works on Seperation of Concerns Principle. Meaning Of Seperation of Concerns is It 
+            Divide Web Development Task into Three Components Models, View and Controller.
+
+            Model : Job of Model is To Represent Your Data From Database.
+            View : Job of View is To Represent the Different HTML Pages as Per Client Requirements
+            Controller : Job of Controller is to Handle Different Kind of Request.
+    
+    2. REST APIs : REST Stands For Repsentational State Transform Protocol. Which Helps to Provide
+            Cross Platform Support for Client and Server. MeanWhile if You Create Server Using 
+            Node.js and express.js using REST API Architecture then One Server handles All Request
+            of Smartphones, Desktop and some Other Kind of Platform Like Android TV etc.
+    
+    3. Micro Services : In Microservices Architecture All Services is Divide into Different Parts.
+            Each Part Have Their Own DataBase, let's suppose one server responsible for authentication
+            of user that server also have their seprate database which only stores account related information
+            and Some Server Which Responsible for Content which also have thier seprate database for
+            content. Like this A Huge Web Applications Devide into seprate Parts and can be manage Easily.            
+            MicroServices Distribute Application into Different Machines, So that it is More Scalable and
+            more high performance. But MicroService Architecture is Complex Compare to Other Two Architecture
+            In this Architecture Copy of Server Also Exist Which Devide Load of One Server into Two Parts
+*/
+
+/*
+Microservices Architecture
+
+What is/are the purpose(s) of the Microservices architecture?
+
+1. It breaks down the application into smaller, independent services that               // Correct
+    communicate with each other.
+2. It increases scalability and maintainability.                                        // Correct
+3. It combines all application services into one big service.
+4. It shares a single database for all services, to ensure seamless integration.
+
+Solution Description : The Microservices architecture approach avoids combining all services 
+                into one large service, which can lead to complexity and create a single point 
+                of failure. The purpose of the Microservices architecture is to structure the 
+                application into small, independent modules. 
+                This enables easier scalability and maintenance without affecting other parts 
+                of the application.
+
+*/
