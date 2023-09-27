11944638669a8a91675560937b5d307361d5d614
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-09-27 21:36:27 +0530

    MCQ Around MVC

---
+++ 04-MVC_Part_I/02-What_is_the_Output.js
@@ -0,0 +1,33 @@
+/*
+Model-View-Controller (MVC)
+
+What is/are the purpose(s) of the Model-View-Controller (MVC) pattern?
+
+1. To separate the application's data, user interface, and control                  // Correct
+    logic.
+2. To promote a clear separation of concerns between different                      // Correct
+    components of the application.
+3. To enable easy modification of the application.                                  // Correct
+4. To combine all components of the application into a single entity.
+
+Solution Description : The purpose of MVC pattern is to separate the application's data, 
+                user interface, and control logic, promoting clear separation of concerns, 
+                maintainable, and organized code. Combining application components into a 
+                single entity is not the purpose of this design pattern.
+*/
+
+/*
+Controller component in the MVC
+
+What is the role of the Controller component in the MVC pattern?
+
+1. It displays the data provided by the model.  
+2. It represents the data and the business logic of the application.
+3. It handles user requests from the view and updates the model and the view.           // Correct
+4. It retrieves and stores data.
+
+Solution Description : The role of the Controller component in the MVC pattern is to 
+            handle user requests from the view and update the model and view accordingly. 
+            It acts as a mediator between the two components, manages the flow of the 
+            application, and facilitates communication between the Model and the View.
+*/
