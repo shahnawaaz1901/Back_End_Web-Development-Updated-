3b6d47301de6c9bcfebabe6fa0c262d3e95c43cf
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-10-02 19:15:14 +0530

    Fixing Some Bugs

--- 04-MVC_Part_I/Inventory_Management/src/controllers/product.controller.js
+++ 04-MVC_Part_I/Inventory_Management/src/controllers/product.controller.js
@@ -1,12 +1,22 @@
 /* Convension of Write Controller is by add .controller next after the name */
 import path from 'path'         // Import Using ES6 Module
 import ProductModel from '../models/product.model.js';
+import { log } from 'console';
 
 // Export Products Controller class Default
 export default class ProductsController{
     // First Function
     getProduct(req, res){
-        console.log(ProductModel.getProducts());
+        let array = ProductModel.getProducts();
+        for(let every of array){
+            console.log(every);
+        }
+        /*
+            Resolve Function Gives us Directory from Root to Current Directory,Now
+            Current Directory Means from Root to File Where Program is Executed, in
+            Out Case index.js is Executed that's Why resolve function gives us from
+            root to Inventory_Management Folder Path not the Path of Product.controller.js
+        */
         let filePath = path.resolve(path.resolve('src','views','products.html'));
         res.sendFile(filePath);
     }
--- 04-MVC_Part_I/Inventory_Management/src/models/product.model.js
+++ 04-MVC_Part_I/Inventory_Management/src/models/product.model.js
@@ -1,19 +1,43 @@
-export default class ProductModel{
-    constructor(_id,_name,_description,_price,_imageURL){
-        this.id = _id;
-        this.name = _name;
-        this.description = _description;
-        this.price = _price;
-        this.imageURL = _imageURL;
-    }
-12
-    static getProducts(){
-        return products;
-    }
+export default class ProductModel {
+  constructor(_id, _name, _description, _price, _imageURL) {
+    this.id = _id;
+    this.name = _name;
+    this.description = _description;
+    this.price = _price;
+    this.imageURL = _imageURL;
+  }
+
+  /*
+    Static KeyWord Helps us to use a function which is inside a class withOut 
+    Creating Object of that Class. We Can Only Use those functions withOut
+    creating an instance of that Class, function which is Creating using 
+    static keyword
+    */
+  static getProducts() {
+    return products;
+  }
 }
 
 var products = [
-    new ProductModel(1, 'Product 1', 'Description for Product 1',19,999,'https://m.media-amazon.com/images/I/51-nXsSRfZL._SX328_BO1,204,203,200_.jpg'),
-    new ProductModel(2, 'Product 2', 'Description for Product 2',29,999,'https://m.media-amazon.com/images/I/51xwGSNX-EL._SX356_BO1,204,203,200_.jpg'),
-    new ProductModel(3, 'Product 3', 'Description for Product 3',39,999,'https://m.media-amazon.com/images/I/31PBdo581fL._SX317_BO1,204,203,200_.jpg'),
-]
+  new ProductModel(
+    1,
+    "Atomic Habits",
+    "A supremely practical and useful book.",
+    300,
+    "https://m.media-amazon.com/images/I/51-nXsSRfZL._SX328_BO1,204,203,200_.jpg"
+  ),
+  new ProductModel(
+    2,
+    "Ikigai",
+    "The Japanese secret to a long and happy life",
+    340,
+    "https://m.media-amazon.com/images/I/51xwGSNX-EL._SX356_BO1,204,203,200_.jpg"
+  ),
+  new ProductModel(
+    3,
+    "Deep Work",
+    "RULES FOR FOCUSED SUCCESS IN A DISTRACTED WORLD",
+    280,
+    "https://m.media-amazon.com/images/I/31PBdo581fL._SX317_BO1,204,203,200_.jpg"
+  ),
+];
