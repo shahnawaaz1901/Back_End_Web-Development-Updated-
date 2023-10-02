09673b1d25683c762ce528bad2c697b1b0b1c99e
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-10-02 22:13:53 +0530

    MCQ Around  Layouts

--- 04-MVC_Part_I/04-Layouts.js
+++ 04-MVC_Part_I/04-Layouts.js
@@ -8,3 +8,23 @@
 
     Layout Works as MiddleWare.
 */
+
+/*
+EJS-Express layouts
+
+Which statement(s) is/are true regarding the use of EJS-Express layouts? ( Multiple )
+
+1. EJS-Express layouts offer a means to create reusable layouts for         // Correct
+    multiple EJS views.         
+2. EJS-Express layouts enable the inclusion of shared elements such         // Correct
+    as headers, footers, and navigation bars in multiple views.
+3. EJS-Express layouts simplify the management of shared components         // Correct
+    across multiple pages.
+4. EJS-Express layouts can improve the organization and maintainability     // Correct
+    of the codebase.
+
+Solution Description : EJS-Express layouts provide reusable layouts for multiple EJS views, 
+                allowing shared elements inclusion. They simplify shared component management, 
+                improve code organization, and enhance application consistency and maintenance.
+
+*/
