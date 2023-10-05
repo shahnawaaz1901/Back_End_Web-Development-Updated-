6509b2de86e22e00f681bf6cb95a6654ac1b4ceb
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-10-05 20:48:15 +0530

    Add Validation on Form

--- 05-MVC_Part_II/Inventory_Management/src/controllers/product.controller.js
+++ 05-MVC_Part_II/Inventory_Management/src/controllers/product.controller.js
@@ -15,6 +15,7 @@ export default class ProductsController{
     getAddProductForm(req, res){
         res.render('new-product',{
             title : 'Add New Product',
+            errorMassage: undefined,
         });
     }
 
@@ -24,6 +25,21 @@ export default class ProductsController{
             we need to decode the data using body parser
         */
         let product = req.body;
+        let errors = [];
+        if(!product.name || product.name.trim() == ''){
+            errors.push("Enter a Valid Name of Product");
+        }
+        if(product.price < 1){
+            errors.push("Price You Entered is Invalid");
+        }
+        try {
+            let url = new URL(product.url);
+        } catch (error) {
+            errors.push("Enter a Valid URL");
+        }
+        if(errors.length > 0){
+            return res.render('new-product',{errorMassage : errors[0],title : 'Add New Product'});
+        }
         ProductModel.addProduct(product.name, product.desc,product.price,product.imageUrl);
         /*
             After Adding New Data Again Fetch the Products and Render it On Browser
