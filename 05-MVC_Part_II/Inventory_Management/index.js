91537a0fb237ac19d6b5741b23f1a13934ee79f1
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-10-13 11:21:01 +0530

    Make Some Minor Changes

--- 05-MVC_Part_II/Inventory_Management/index.js
+++ 05-MVC_Part_II/Inventory_Management/index.js
@@ -9,21 +9,25 @@ const server = express();
 server.set('view engine','ejs');                    
 server.set('views',path.join('src','views'));       
 
-// Use MiddleWare to render Static Files on Views Folder
+//* Use MiddleWare express.static to Render the css and js file
 server.use(express.static(path.join('src','views')));
 
 // Use Layouts 
 server.use(expressEjsLayouts);
 
-// urlEncoded as Middleware to encoded the data into the urlEncoded
+// urlEncoded as Middleware to decode the data which browser sent from the user
 server.use(express.urlencoded({extended : true}));
+
 // For Use a function which is Inside in Class we Need to Create Object of that Class
 let products = new ProductsController();
 
+//* Setup the Router 
 server.get('/',products.getProduct);
 server.get('/new-product',products.getAddProductForm);
 server.get('/update-product',products.getUpdateProductView);
 server.post('/',validate, products.addNewProducts);
+
+//? Listen the Server at PORT 3200
 server.listen(3200,function(err){
     if(err){
         console.log(`Error : ${err}`);
