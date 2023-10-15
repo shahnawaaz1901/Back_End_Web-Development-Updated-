250461cce12962b427b89dd7fc8e37ab3171a959
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-10-15 19:59:49 +0530

    Create UserModel to Store the User Information

---
+++ 06-MVC_Part_III/Inventory_Management/src/models/user.model.js
@@ -0,0 +1,10 @@
+export default class UserModel{
+    //*Constructor
+    constructor(_userId,_name, _email,_password){
+        this.userId = _userId;
+        this.name = _name;
+        this.email = _email;
+        this.password = _password
+    }
+
+}
