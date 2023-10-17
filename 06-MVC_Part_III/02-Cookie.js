140119ab90a8731b2ea362a4a9684c2947974c07
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-10-17 13:04:24 +0530

    UnderStand Cookie

---
+++ 06-MVC_Part_III/02-Cookie.js
@@ -0,0 +1,32 @@
+/*
+ * Cookie is a Storage System which Stores Some data on the Browser
+ 
+ ?When Ever We Create a Sesssion that Session Id is Sent by the Server 
+ ?to the Client and Client Stores that Session Id into Cookie and Sent 
+ ?back to the server along with Every Request
+
+ !We Can Create our Own Cookies in the Browser
+
+ *Cookie is Used by every big web application Like amazon uses cookie to
+ *store what was you last logged in what was the last product checkout by
+ *the user.
+*/
+
+/*
+*Cookies
+
+*Which statement(s) is/are true about cookies?
+
+1. Cookies are stored on the user's browser.                    //*Correct
+2. Cookies have a longer lifespan compared to sessions.         //*Correct
+3. Cookies are only used for authentication purposes.
+4. Cookies are used to track user behavior on websites.         //*Correct
+
+Solution Description : Cookies are small pieces of data that are stored on 
+            the user's browser. They have a longer lifespan compared to sessions, 
+            as they can be set with expiration dates. While cookies are commonly 
+            used for tracking user behavior on websites, they serve various other 
+            purposes beyond authentication. For example, they store user preferences 
+            and enable personalized experiences.
+ 
+*/
