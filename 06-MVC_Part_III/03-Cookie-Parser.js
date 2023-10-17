ea25e70065da366090e0b78c11c1441c67b0dc5b
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-10-17 13:21:27 +0530

    UnderStand Cookie Parser

---
+++ 06-MVC_Part_III/03-Cookie-Parser.js
@@ -0,0 +1,6 @@
+/*
+    Cookie-Parser : Cookie Parser Parse Cookie header and Populate req.cookie
+                with an object keyed by the cookie names. Optionally You May
+                enable signed cookie support by parsing a secret string which
+                assigns req.secret so it may be used by other middlewares.
+*/
