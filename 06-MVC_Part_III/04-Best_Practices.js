267b4e9dd8810f64b8310f7d99850c35ce64c3d0
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-10-17 18:57:17 +0530

    UnderStand Best Practices for MVC Application

---
+++ 06-MVC_Part_III/04-Best_Practices.js
@@ -0,0 +1,35 @@
+/*
+ * --Best Pecitces-- *
+*/
+
+/*
+ ?1. Sepration of Concerns : Seprate Data, Handler and View in Model, View and Controller. 
+ !Do not Load Controller for Multiple Responsibilities
+ ?2. DRY : Dont Repeat YourSelf means Try to ReUsed Some Code as Possible
+ ?3. Using Middlewares : It DepCouples or Reduce Complexities from Controller
+ ?4. Modularize : Seprate Your Entire Application into chunks or Sepration File
+ ?5. Naming Conventions : Try to Give Name as Per work of functions, variables or Modules(if Internally Created)
+ ?6. Security Implementation : Authorize,Authentication and Validation Used in Application.
+*/
+
+/*
+Best practices for MVC Applications
+
+Which of the following are best practices for Model-View-Controller (MVC) 
+applications?
+
+1. Combining models, views, and controllers into a single 
+    class for simplicity.
+2. Breaking the application into smaller, reusable modules.             //*Correct
+3. Avoiding the use of middleware functions for request 
+    processing.
+4. Implementing security measures, such as using secure                 //*Correct
+    communication protocols and validating user input.
+
+Solution Description : Breaking the application into smaller, reusable modules 
+                improves maintainability. Implementing security measures, such 
+                as secure communication protocols and input validation, protects 
+                the application. Combining models, views, and controllers into a 
+                single class leads to code complexity while avoiding middleware 
+                functions limits functionality and common task handling.
+*/
