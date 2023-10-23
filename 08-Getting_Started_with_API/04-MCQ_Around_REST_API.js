03445ea4af7332d03c76e77fa3f4074a02d90913
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-10-23 21:36:14 +0530

    MCQ Around REST API

---
+++ 08-Getting_Started_with_API/04-MCQ_Around_REST_API.js
@@ -0,0 +1,57 @@
+/*
+RESTful APIs
+
+What is one of the key characteristics of RESTful APIs?
+
+1. They store client information in sessions for improved security.
+2. They are built upon a stateful architecture, ensuring efficient communication.
+3. They allow for seamless integration with various platforms                   //*Correct
+4. They primarily focus on rendering views and controlling the UI.
+
+Solution Description : The correct statement is C. One of the primary 
+                attributes of RESTful APIs is their capability to enable 
+                seamless integration with diverse platforms and systems. 
+                RESTful APIs enjoy extensive support across various types 
+                of platforms, facilitating smoother communication between 
+                servers and clients.
+*/
+
+/*
+REST API and scalability
+
+How does REST API contribute to the scalability of applications?
+
+1. By tightly coupling the backend API with the client for efficient scaling.
+2. By replicating databases across multiple servers.
+3. By relying on session-based architecture to handle increasing user requests.
+4. By being loosely coupled and independent of clients, facilitating easy scaling.      //*Correct
+
+Solution Description : The correct answer is D. REST APIs contribute to the 
+                    scalability of applications by being loosely coupled and 
+                    independent of clients. This means that the backend API 
+                    can be scaled independently without affecting the clients. 
+                    It becomes easier to replicate servers or add more resources 
+                    to handle increased traffic since the server does not store 
+                    client-specific information.
+*/
+
+/*
+HTTP methods used in REST APIs
+
+Which statement(s) correctly describe(s) the functions of HTTP methods 
+used in REST APIs? (Multiple)
+
+1. GET method is used to modify an existing resource, and 
+    POST method is used to fetch details of a resource.
+2. POST method is used to fetch details of a resource, and 
+    PUT method is used to delete a resource.
+3. GET method is used to fetch details of a resource,                   //*Correct
+    POST method is used to create a new resource.
+4. PUT method is used to update a resource, and                         //*Correct
+    DELETE method is used to remove a resource.
+
+Solution Description : Statements C and D are correct as they accurately describe 
+                the functionalities of RESTful methods. GET retrieves information 
+                about a resource, POST creates new resources, PUT modifies existing 
+                resources, and DELETE removes them.
+*/
