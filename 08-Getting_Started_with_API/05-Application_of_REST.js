9b767dbd063b2e3f195499b644b9183a4e1bad4c
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-10-23 21:55:03 +0530

    UnderStand Applications of REST APIs

---
+++ 08-Getting_Started_with_API/05-Application_of_REST.js
@@ -0,0 +1,18 @@
+/*
+    REST is the Most Popular Architecture to Create a Scalable Application
+*/
+
+/*
+    Application of REST APIs :
+    1. Third Party Integration : For Use Other Company's API to retreive the 
+        Data that Company Also Have the REST API so that We Can Retreive the 
+        Resource Independently. Meaning of third Party API is to USED other
+        company REST API to retreive the data and Displayed on Our Application.
+    
+    2. Backend APIs : Create for CRUD operations on Cross platform using an 
+        single API for Each Operation. Like Mobile Devices, Laptop, Mac or IOS.
+    
+    3. Micro Services : With REST We Can Create a backend API Where Client 
+        Service and Admin Service works Seprately With the Help of an API
+        Gateway. 
+*/
