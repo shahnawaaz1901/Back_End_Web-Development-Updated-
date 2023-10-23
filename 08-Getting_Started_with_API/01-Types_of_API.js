79a7d6976d87728835c4edfc35f1d27e1ef6fde6
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-10-23 19:25:36 +0530

    UnderStand API Types

---
+++ 08-Getting_Started_with_API/01-Types_of_API.js
@@ -0,0 +1,29 @@
+/*
+    Three Types of API is Exist : 
+    1. SOAP API
+    2. REST API
+    3. GRAPH-QL
+*/
+
+/*
+    1. SOAP (Simple Object Access Protocol) : SOAP is the OLDEST Method of API to
+                                Retreive the Data from the Server to Client. SOAP
+                            is Currently Rarely Used Because it's Very OLD and 
+                        Approx nothing used with the Node.JS. SOAP is Used to 
+                    Handle API Request of the Client but the Biggest Problem with
+                the SOAP is it's very heavy and it's bulky to Transfer Data to the
+            Client. Due to HeavyNess SOAP is Very Slow also and it's Very Complex
+        to UnderStand Compare to REST API Because REST API is Very Simplified.
+
+    
+    2. REST API : REST API is the Most Popular Way to Communicate Between Client
+            and the Server. REST API is the Most Widely Used and Widely Supported
+            Across the Web. 
+    
+
+    3. GRAPH-QL : Graph-QL is the new technology and it's get more popularity in
+            less time. GraphQL tried to Reduce the Load while Communication b/w
+        Client and the Server. GraphQL tried to get data from the Server which
+        is necessory to Client. GraphQL not Transfer unNecessory data or Info
+        that's Why it's Reduce the Loaded and Graph QL is Also Very Fast.
+*/
