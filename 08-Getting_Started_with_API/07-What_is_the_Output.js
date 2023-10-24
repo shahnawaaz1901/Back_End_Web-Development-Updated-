338a96a8aafbddbb17a28ffb48deb3163e80676f
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-10-24 18:50:43 +0530

    MCQ Around Collabration

---
+++ 08-Getting_Started_with_API/07-What_is_the_Output.js
@@ -0,0 +1,19 @@
+/*
+Access and Editing Permissions
+
+In a collaborative project management tool, how can you limit access to 
+sensitive project files and restrict editing permissions to specific team members? (Multiple)
+
+1. By implementing an authentication and authorization system.              //*Correct
+2. By creating a separate file storage system for sensitive files.
+3. Utilize role-based user management, granting editing permissions         //*Correct
+    only to team members assigned specific roles or responsibilities.
+4. Implement regular data backups and version control to mitigate the 
+    risk of unauthorized modifications.
+
+Solution Description : By implementing an authentication and authorization system 
+                with role-based access control. Role-based user management grants 
+                specific editing permissions to team members based on their roles 
+                or responsibilities, limiting access to sensitive project files and 
+                ensuring authorized collaboration within the project management tool.
+*/
