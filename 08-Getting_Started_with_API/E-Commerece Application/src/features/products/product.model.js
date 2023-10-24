ebfceeef6cf07f059d42f815fbfaad6223978ed4
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-10-24 21:30:10 +0530

    Adding More detail on Models

--- 08-Getting_Started_with_API/E-Commerece Application/src/features/products/product.model.js
+++ 08-Getting_Started_with_API/E-Commerece Application/src/features/products/product.model.js
@@ -1,5 +1,5 @@
 export default class ProductModel {
-  constructor(_id, _name, _description, _imageURL, _category, _price, _sizes) {
+  constructor(_id, _name, _description, _price, _imageURL, _category, _sizes) {
     this.id = _id;
     this.name = _name;
     this.description = _description;
@@ -15,7 +15,31 @@ export default class ProductModel {
 }
 
 var products = [
-    new ProductModel(1,"Product 1","Description for Product 1"),
-    new ProductModel(2,"Product 2","Description for Product 2"),
-    new ProductModel(3,"Product 3","Description for Product 1"),
+  new ProductModel(
+    1,
+    "Product 1",
+    "Description for Product 1",
+    2400,
+    "https://m.media-amazon.com/images/I/51-nXsSRfZL._SX328_BO1,204,203,200_.jpg",
+    "Category 1",
+    ["S","M","L","XL"]
+  ),
+  new ProductModel(
+    2,
+    "Product 2",
+    "Description for Product 2",
+    3566,
+    "https://m.media-amazon.com/images/I/51xwGSNX-EL._SX356_BO1,204,203,200_.jpg",
+    "Category 2",
+    ["S","M","L","XL"]
+  ),
+  new ProductModel(
+    3,
+    "Product 3",
+    "Description for Product 1",
+    6454,
+    "https://m.media-amazon.com/images/I/31PBdo581fL._SX317_BO1,204,203,200_.jpg",
+    "Category 3",
+    ["S","M","L","XL"]
+  ),
 ];
