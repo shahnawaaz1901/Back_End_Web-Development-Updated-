83281781a14202db736b7664bc835cb0517c3dd4
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-10-26 20:11:52 +0530

    Import the Code of ECOM App

---
+++ 09-Securing_APIs/E-Commerece Application/index.js
@@ -0,0 +1,28 @@
+//* Import Modules
+import express from "express";
+import ProductRouter from "./src/features/products/product.router.js";
+import bodyParser from "body-parser";
+import upload from "./src/middlewares/file-upload.js";
+
+//* Start the Server
+const app = express();
+
+//* Body Parser Use to Get Data on req.body
+app.use(bodyParser.json());
+
+//* Its Good practice to write the /api first when we Create Route for API
+app.use("/api/products", ProductRouter);
+
+//* Default Route
+app.get("/", function (req, res) {
+  res.send("Welcome to API Application !!");
+});
+
+//* Listen the Server on Port 3200
+app.listen(3200, function (err) {
+  if (err) {
+    console.log(`Error While Run the Server : ${err}`);
+    return;
+  }
+  console.log(`Server is up and Run on Port 3200`);
+});
---
+++ 09-Securing_APIs/E-Commerece Application/src/features/products/product.controller.js
@@ -0,0 +1,50 @@
+import ProductModel from "./product.model.js";
+import path from 'path';
+export default class ProductController {
+  //* Get All Products
+  getProducts(req, res) {
+    const product = ProductModel.getAll();
+    res.status(200).send(product);
+  }
+
+  //* Add New Product
+  addProduct(req, res) {
+    const { name, price, sizes } = req.body;
+    const imageURL = req.file.filename;
+    const newProduct = {
+        name,
+        price : parseFloat(price),
+        sizes : sizes.split(','),
+        imageURL,
+    }
+    ProductModel.addProduct(newProduct);
+    res.send(ProductModel.getAll());
+    // res.send("Your Post Request is Received !! Very ThankYou");
+  }
+
+  //* Get Product by ID
+  getOneProduct(req, res) {
+    const id = req.params.id;
+    const product = ProductModel.getProductById(id);
+    if(!product){
+      res.status(404).send("Product not Found !!");
+    }else{
+      res.status(200).send(product);
+    }
+  }
+
+  //* Filter Products
+  filterProducts(req, res){
+    //* All Query Parameters data is inside the request.query Object
+    const {minPrice, maxPrice, category} = req.query;
+    const result = ProductModel.filter(minPrice, maxPrice, category);
+    if(!result){
+      res.status(404).send("Sorry No Products Found !!");
+    }else{
+      res.status(200).send(result);
+    }
+  }
+
+  //* Rate the Product
+  rateProduct(req, res) {}
+}
