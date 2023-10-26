c081e6580ef4f3424e98a0924358162a2ed0e1b3
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-10-26 20:01:31 +0530

    MCQ Around Authentication and Authorization

---
+++ 09-Securing_APIs/01-What_is_the_Output.js
@@ -0,0 +1,38 @@
+/* 
+OAuth (Open Authorization)
+
+Suppose an application relies on Facebook's authentication system to verify its users. 
+What is/are the advantage(s) of using this approach? ( Multiple )
+
+1. Enhanced security due to Facebook's robust verification process          //* Correct
+2. Compatibility with a wide range of devices and platforms                 //* Correct
+2. Increased user trust and familiarity with Facebook's brand               //* Correct
+4. Offline support to verify identity with Facebook authentication.
+
+Solution Description : Relying on Facebook's authentication system offers enhanced security, 
+                and compatibility with various devices and platforms, and increases user trust 
+                and familiarity with the Facebook brand.
+*/
+
+/*
+Authentication and Authorization
+
+What is the primary difference between authentication and authorization in the context of 
+user access control?
+
+1. Authentication ensures the integrity of user data, while authorization determines user 
+    permissions.
+2. Authentication verifies the identity of a user, while authorization controls the user's              //* Correct
+    access to resources.
+3. Authentication ensures secure transmission of user credentials, while authorization 
+    encrypts user data.
+4. Authentication establishes a secure connection between the user and the server, while 
+    authorization manages user session timeouts.
+
+Solution Description : Authentication is the process of verifying the identity of a user, 
+                ensuring that the user is who they claim to be. It involves validating user 
+                credentials, such as usernames and passwords. On the other hand, authorization 
+                determines the permissions and privileges that a verified user has once their 
+                identity has been established. It controls the user's access to specific resources, 
+                functionalities, or data within the system.
+*/
