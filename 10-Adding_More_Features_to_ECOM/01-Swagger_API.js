5975611797ab3a7ed27ab1347779c8fed3a84c90
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-11-02 20:45:02 +0530

    MCQ Aroud Swagger UI

---
+++ 10-Adding_More_Features_to_ECOM/01-Swagger_API.js
@@ -0,0 +1,14 @@
+/*
+What is the Main Purpose of Using Swagger UI in Node.js Application ..?
+
+1. To implement authentication mechanisms for users.
+2. To create UI components for frontend applications.
+3. To provide better documentation and enhance consumer experience for APIs.        //* Correct
+4. To generate random API documentation for Node.js applications.
+
+Solution Description : Swagger UI is used in a Node.js API application to 
+                provide improved documentation and enhance consumer experience 
+                for APIs. It offers an interactive, user-friendly interface 
+                that allows consumers to understand and test API functionalities 
+                effectively.
+*/
