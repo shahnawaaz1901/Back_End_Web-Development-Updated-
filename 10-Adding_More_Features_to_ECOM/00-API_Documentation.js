8b0f7a76c9a497286a4afacd8a59d01c3315ed63
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-11-02 19:32:55 +0530

    Understand Importance of Documentation in API

---
+++ 10-Adding_More_Features_to_ECOM/00-API_Documentation.js
@@ -0,0 +1,44 @@
+/*
+    When Ever We Create an API we might not Know who is Our Client and What
+    Data Client sends to the server.
+
+    For This type of Issue We Need to Create an API Documentation Tool Which 
+    Helps the Client to UnderStand How our API is Work and What Kind of Data
+    we Expect from the Client. How Client Can Call the API and Which Data our
+    API is sent.
+    
+    We can use OPEN API tool Which Provides a standardized means to defne Your
+    API to Others.
+
+    with API Documentation we Can Quickly UnderStand how API Works, configure the
+    infrastructure, generate client codes and test cases for the API. All the
+    Feature is Part of Documentation.
+
+    'SWAGGER' is tool to Implement the All of the Specification of API 
+
+    When We Book an Appointment for Covid Vaccine we can Book Appointment for 
+    Vaccination from Many Apps like paytm, arogyasetu but how can we Book appointment 
+    for Vaccination this all can done by the API which government of India provides
+    for All Online platforms. That API is Helps us to book appointment for covid
+    vaccination from Multiple Platforms.
+    
+    When we Search for Cowin Vaccination API in we get multple things like an API
+    key and how can we use that api in our application to implement the appointment
+    feature in Our Applcation. And What Kind of Request API expect from the user.
+
+    With API Documentation of Covid Vaccination, Government Provide All Details what
+    they expect from the client and also provide the response for every request
+    status of response for every request. All the things goverment provide so that
+    if Anyone want to use that api in application can be Implement by UnderStand
+    that API with the Help of the Documentation 
+*/
+
+/*
+What is the Purpose of API Documentations ..?
+
+1. To help clients understand the inner workings of an API.
+2. To provide a standardized means of defining APIs for servers.        //* Correct
+3. To ensure the API is secure and immune to cyber-attacks.
+4. To generate client code automatically.
+
+*/
