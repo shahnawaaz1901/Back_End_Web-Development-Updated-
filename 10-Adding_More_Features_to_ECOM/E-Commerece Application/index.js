b468dbe130fe567438595b7a7f587a2f26db6a8c
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-11-03 22:02:13 +0530

    Implement the CORS Policy to Make Request to Server by Client

--- 10-Adding_More_Features_to_ECOM/E-Commerece Application/index.js
+++ 10-Adding_More_Features_to_ECOM/E-Commerece Application/index.js
@@ -16,25 +16,71 @@ import jwtAuth from "./src/middlewares/jwt.middleware.js";
   explicitly that this is a json object by writing an assert keyword and add
   an object and put type value is json in that object
 */
-import apiDocs from "./swagger.json" assert {type : 'json'};
+import apiDocs from "./swagger.json" assert { type: "json" };
 
 //* Start the Server
 const app = express();
+
+app.use((req, res, next) => {
+  /* 
+    For Allow Our Server to Serve response to Different Origin We Need to set
+    response header access-control-allow-origin to that perticular origin link
+    which we want to allow to access the data after the API Call Which is Done
+    By the Client Side.
+  */
+  /* 
+    We Always need to Speicify Access Controll to the Response Object no the 
+    Req Object because it's the Server responsibiliy for response it's not 
+    Client responsibility thats why we Only need to specify access control to
+    the response Object 
+  */
+
+  //* Response header for all Origins withOut restrictions
+  //* res.header("Access-Allow-Control-Origin", "*");
+
+  //* Response Header for a specific Different Origin
+  res.header("Access-Control-Allow-Origin", "http://localhost:3200");
+    
+  /*
+    We Also need to configure what all type of headers client can send as part of
+    CORS policy 
+  */
+  app.use("Access-Control-Allow-Header","content-type, authorization");
+
+  /*
+    Along with this we Also Need to Specify that what kind of http methods allows
+    to the Client to make API Call to the Server default value for the request
+    method type is '*' which means All type of Request Methods Allow to Client
+  */
+  app.use("Access-Control-Allow-Method","*");
+  /*
+    Preflight : Preflight is the verification request that ensures does server
+            needs to response this request. Why Browser sent the preFlight Request
+            because we have pass a header which is authorization header for 
+            secure our APIs. In preFlight request browser sent request to the server
+            which http request method is options which we Not Specified We Specify
+            Only get and Post Request method that's why we need to sent a response 
+            to the browser with ok massage  
+  */
+  if(req.method == "OPTIONS"){
+    res.sendStatus(200);
+  }
+  next();
+});
 
 //* SetUp Cookie Parser
 app.use(cookieParser());
 
 //* Body Parser Use to Get Data on req.body
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
 /* 
   Swagger.serve create interface documentation and and swagger.setup pass json for 
   render on the screen
 */
-app.use("/api-docs", swagger.serve, swagger.setup(apiDocs))
-app.use("/api/products",jwtAuth,productRouter);            
-app.use("/api/users",userRouter);
+app.use("/api-docs", swagger.serve, swagger.setup(apiDocs));
+app.use("/api/products", jwtAuth, productRouter);
+app.use("/api/users", userRouter);
 app.use("/api/cart", jwtAuth, cartRouter);
 //* Default Route
 app.get("/", function (req, res) {
@@ -55,12 +101,13 @@ app.get("/", function (req, res) {
   parses bodyParser sessions this all middlewares not depend on any routes that's why 
   this middleware always executes on Any Request. 
 */
-app.use((req, res)=>{
+app.use((req, res) => {
   res.status(404).send({
-    status : "failure",
-    massage : "API Which You Looking for Is Not Exist, Please Visit Our API Documentation on http://localhost:3200/api-docs/ for More Details"
-  })
-})
+    status: "failure",
+    massage:
+      "API Which You Looking for Is Not Exist, Please Visit Our API Documentation on http://localhost:3200/api-docs/ for More Details",
+  });
+});
 
 //* Listen the Server on Port 3200
 app.listen(3200, function (err) {
