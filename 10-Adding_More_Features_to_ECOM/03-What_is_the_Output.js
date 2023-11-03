55e63c93815db6c38b43406268e04a9cb59a5d67
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-11-03 19:45:00 +0530

    MCQ Around Swagger 3.0

---
+++ 10-Adding_More_Features_to_ECOM/03-What_is_the_Output.js
@@ -0,0 +1,44 @@
+/*
+OpenAPI 3.0 Advantages
+
+What is the main advantage of using OpenAPI 3.0 over Swagger 2.0.?
+1. Improved server performance.
+2. Better handling of request parameters.
+3. Enhanced specification of media types and components.        //* Correct
+4. Simpler URL management.
+
+Solution Description : Open API 3.0 Provides more options to specify components, 
+                such as model schemas and detailed media types, which allows for 
+                better documentation and handling of data in the API.
+*/
+
+/*
+Replacing Request Parameters
+
+In OpenAPI 3.0 what has replaced the "parameters" object to specify the request .?
+
+1. parameters
+2. requestParams
+3. request
+4. requestBody          // *Correct
+
+Solution Description : In OpenAPI 3.0, the "parameters" object has been replaced by 
+                "requestBody" to specify the data sent in a request.
+*/
+
+
+/*
+Configure Multiple Servers
+
+Which Section should you modify in your API Documentation to specify multiple server 
+configuration in Open AI 3.0..?
+
+1. Paths
+2. Schemas
+3. Components
+4. Servers              //* Correct
+
+Solution Description : In Open AI 3.0 you can specify multiple server configuration 
+                under the "servers" section, allowing you to define different URLs 
+                for Various Environments.
+*/
