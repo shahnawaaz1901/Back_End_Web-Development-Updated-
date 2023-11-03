9643a28d2fab8ff9d90b3cb37c09e1a7f8e29e30
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-11-03 21:11:22 +0530

    UnderStand CORS Policy

---
+++ 10-Adding_More_Features_to_ECOM/04-CORS_Policy.js
@@ -0,0 +1,15 @@
+/* 
+    CORS : CORS Stand for Cross Origin Requests.This Means When We Run the Server
+        on a PORT if we Our Client Which Runs UI on Different PORT and if that 
+        Client sends request to the Server then that Client Not Able to fetch the
+        data and an error is Occur. This is the CORS Policy Which Means Cross 
+        Origin Requests means origin of both Client and Server is Different.
+        
+    FIX the CORS Policy Error : 
+    We Can Config the In our Server and Allow Different Origin Client to Access
+    and fetch the data from the server. This is All We Can done by Config the
+    server file for the Client So that we can able to fetch the API data from
+    different Origin(here Origin Stands for the Port which we Can Use to Run the
+    Server and as well as when We use Live server that can also use the port of
+    the system )
+*/
