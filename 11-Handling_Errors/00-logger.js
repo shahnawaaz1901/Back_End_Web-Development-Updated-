75f9bb5854ebc3238cb5737640bb8d9061b96bf3
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-11-04 19:28:02 +0530

    Introduction to Logger

---
+++ 11-Handling_Errors/00-logger.js
@@ -0,0 +1,9 @@
+/*
+    What is Logger and Why Logger is Important..?
+
+    Logger is a file which Contains All Error related Information While
+    Running a server for every request. So that Log is Important for us
+    to keep track on the Errors. This Log File Helps us to Identifying
+    errors and as well as at which type of Request error occurs . We 
+    Can Use logger by Adding a logger middleware for every request .
+*/
