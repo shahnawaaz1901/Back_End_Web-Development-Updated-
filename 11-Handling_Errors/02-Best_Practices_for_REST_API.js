a446d650326df661773752559d323d3686e70f8d
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-11-07 21:11:48 +0530

    Learn Best Practices for REST APIs

---
+++ 11-Handling_Errors/02-Best_Practices_for_REST_API.js
@@ -0,0 +1,10 @@
+/* //*Best Practices While Working With REST APIs// */
+/*
+    ?1. Using Correct Status Codes
+    ?2. Usin Correct http request Methods
+    ?3. Proper Error Handling
+    ?4. Secured APIs
+    ?5. Input Validation
+    ?6. Use Naming Conventions for URL                          //* Read REST API Docs
+
+*/
