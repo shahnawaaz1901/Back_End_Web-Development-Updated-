bd8dcc8cfa9774e57eeb2b52f75219bdd9cf06f9
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-11-08 20:05:58 +0530

    UnderStand Data and Its Importance

---
+++ 13-Getting_Started_with_Database/00-UnderStanding_Data.js
@@ -0,0 +1,38 @@
+/*
+    What is Data : Data can be file, image or anything which provide some 
+                Information to us.
+    
+    Right Now in Our E Commerce Application If We want to store some data on
+    our server that data is destroyed when we restart the server. Only data
+    which is inside the model is available to us. For Storing data we need
+    to use persistent storage right now we use in memory storage that's why
+    our data destroyed when we restart the server.
+    
+    Importance of Data : Application withOut Data is useless because if some
+                Web Application or Android Application Not Probide any kind
+                of Data then No Person use that application
+    
+    In Our E Com API Application We store Data Product, Cart, rate that Data
+    is Helps us to track our Information regarding ecom API. User Credentials
+    also a data which is Most Important and We Also Need to Store User Creden
+    tials after the encryption so that it provides more security and safety
+
+    Our File System is Also is Persistent Storage but question is all big brand
+    stores their data into the file system we can say yes but all brand not use 
+    file system directly but use indirectly through the databases. Because in 
+    File System it's hard to manage the data update the data through file system
+    that's why all brands uses databases to store the data. Database uses file
+    system to store data but database provides us more convinient way to store 
+    or manipulate the data and Managing Data in Database is easy through the
+    Database. Using Persistent storage if our server is restart all the Data which
+    we store while running the server is not destroyed.
+
+    File System Not Allows Us to store data in convenient way that's why we use
+    databases and Data Related Operation CRUD in file system is hard to do but
+    in the databases it provides convenience to do All Operations.
+
+    We Can UnderStand databases as tool because using databases gives us data as 
+    per requirements means data which is required for the user is sent by the data
+    bases. 
+
+*/
