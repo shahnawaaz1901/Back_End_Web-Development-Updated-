9e82a75977ce62d9bc553e47510d34405d4742b8
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-11-09 19:09:53 +0530

    MCQ Around databases

---
+++ 13-Getting_Started_with_Database/03-What_is_the_Output.js
@@ -0,0 +1,30 @@
+/*
+Database Misconsceptions
+
+Which of the followinf statements about database is true .? ( Multiple )
+
+
+1. Databases are software that persistently store data.             //* Correct
+2. Databases support various data management operations             //* Correct
+    such as insertion, deletion, updating, and retrieval of data.
+3. A database schema describes the organization and structure 
+    of the data in the database.
+4. Databases can only store text and numeric data.
+*/
+
+/*
+
+Relating Comments to Post in NoSQL
+
+Your Are Building a scocial media platform using NoSQL database. 
+Each post can have multiple comments. What are the possible ways 
+to relate Comments to a specific post .? ( Multiple )
+
+
+1. Store each comment as a separate document with the post ID.      //* Correct
+2. Store each comment as a nested object inside the post document.  //* Correct
+3. Employ a separate NoSQL collection for comments, treating each 
+    comment as an independent document without any reference to posts.
+4. Store the user’s post as a nested object inside the comment 
+    document.
+*/
