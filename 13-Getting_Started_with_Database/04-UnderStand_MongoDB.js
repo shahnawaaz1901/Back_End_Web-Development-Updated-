12a0d78f7c79913d4365efe5954192653accdf5d
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-11-09 19:57:03 +0530

    UnderStand More on MongoDB

---
+++ 13-Getting_Started_with_Database/04-UnderStand_MongoDB.js
@@ -0,0 +1,16 @@
+/* 
+    In Relational Databases in a Database we have different datasets or tables this 
+    and in every table we have rows and columns row have a data of a person but in
+    NoSQL Database Table is know as collection and every data of row is known as document
+    A collection is collection of documents.
+
+    In NoSQL Database every database can have different collections and every collection
+    is the Collection of multiple Documents .
+    
+    In today scenario we cant say that applications relay on single database because 
+    we cant predict that what kind of data user can sent also we can't say that every
+    application uses NoSQL Databases because in some scenario application requires fix
+    data like in case of user credentials application requires fix email phone number and
+    password and this things can't be modified so we can say in todays scenario both NoSQL
+    and Relational databases is used by the Companies which has huge data of users 
+*/
