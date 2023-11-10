e5760a2a0c6500c1007c2fdc62861a4f05eb92a1
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-11-10 21:18:20 +0530

    MCQ Around MongoShell Command

---
+++ 13-Getting_Started_with_Database/06-What_is_the_Output.js
@@ -0,0 +1,47 @@
+/* Mongo DB Database Creating/Switch Commands 
+
+Which of the following set of commands should be used to create or switch to a 'sales' database in MongoDB .?
+
+1.                                  //* Correct
+mongosh
+use sales
+
+2.
+mongosh
+show sales
+
+3.
+mongosh 
+create db sales
+
+4. 
+mongosh
+db.create('sales')
+use sales 
+*/
+
+/*
+MongoDB Multiple Inserts
+
+How you can insert multiple records into the 'transactions' collection of the 'iPay' database using MongoDB.?
+
+1.
+use iPay
+db.transactions.insertMany([{transactionId : "002", userId : "U101", ammout : 500},{transactionId : "001", userId : "U100", ammout : 1000}])
+
+2.
+use iPay
+db.transactions.insertMany({transactionId : "002", userId : "U101", ammout : 500},{transactionId : "001", userId : "U100", ammout : 1000})
+
+3. 
+use iPay
+db.transactions.insertMany({transactionId : "002", userId : "U101", ammout : 500}*5])
+
+4.
+use iPay
+db.transactions.insertOne([{transactionId : "002", userId : "U101", ammout : 500},{transactionId : "001", userId : "U100", ammout : 1000}])
+
+
+
+
+*/
