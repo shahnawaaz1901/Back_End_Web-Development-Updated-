11c55076620a391645609697af9a821587ee0906
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-11-10 11:55:21 +0530

    SetUp the Routes and Adding Comments

--- 12-Mini_Project/postaway/index.js
+++ 12-Mini_Project/postaway/index.js
@@ -1,7 +1,24 @@
+//** Import Nessesory Modules */
+
+//? Packages or External Modules
 import express from 'express';
 
+//? Internal Modules
+import usersRouter from './src/features/users/users.router.js';
+import postsRouter from './src/features/posts/posts.router.js';
+import likesRouter from './src/features/likes/likes.router.js';
+import commentsRouter from './src/features/comments/comments.router.js';
+
+//* Start the Server
 const server = express();
 
+//* Setting Up Routes
+server.use('/api/users',usersRouter);
+server.use('/api/posts',postsRouter);
+server.use('/api/likes',likesRouter);
+server.use('/api/comments',commentsRouter);
+
+//* Listen the Server
 server.listen(3200, (err)=>{
     if(err){
         console.log(`Error While Listening the Server : ${err}`);
