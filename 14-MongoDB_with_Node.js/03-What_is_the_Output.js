441078b3d78b36150f326f469555a1974f6b5af4
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-11-13 00:40:44 +0530

    MCQ Around Repository file

---
+++ 14-MongoDB_with_Node.js/03-What_is_the_Output.js
@@ -0,0 +1,17 @@
+/*
+Repository Purpose
+
+What is the responsibility of the repository module in a Node.js application .?
+
+
+1. To abstract the data access code and centralize it.                      //* Correct
+2. To integrate third-party APIs more seamlessly.
+3. To transform raw data into different formats.
+4. To set environment variables for the application.
+
+Solution Description : The repository module in a Node.js application is primarily 
+                designed to server as an abstraction layer over the data access code. 
+                This means that instead of having database-related code scattered 
+                throughout the application, it is centralized within the repository.
+
+*/
