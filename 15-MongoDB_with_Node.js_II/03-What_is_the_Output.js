47053cfec00904cc2e1caaa582207d4a24a09298
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-11-17 18:35:44 +0530

    UnderStand About Indexing in MongoDB

---
+++ 15-MongoDB_with_Node.js_II/03-What_is_the_Output.js
@@ -0,0 +1,14 @@
+/*
+Indexing Behavior in MongoDB
+
+What is the default indexing behavior for MongoDB when creating an 
+index .?
+
+1. Sparse indexing
+2. Unique indexing
+3. Compound indexing
+4. Ascending indexing                   //* Correct
+
+Solution Description : by default, MongoDB created ascending indexes 
+                unless specifies otherwise.
+*/
--- 15-MongoDB_with_Node.js_II/E-Commerece Application/src/config/mongodb.js
+++ 15-MongoDB_with_Node.js_II/E-Commerece Application/src/config/mongodb.js
@@ -27,3 +27,29 @@ const createCounter = async (db) => {
     await db.collection("counters").insertOne({ _id: "cartItemId", value: 0 });
   }
 };
+
+/*
+  In a Huge Dictionary if we Search a word for the meaning in the Dictionary
+  then we should't find the word directly One by One by searching the word in
+  each page. For Efficient Searching We See a page where All Words Start Word
+  is mentioned along with its page number. So that we match first character of
+  word which we want to search and find the page number of that character , so
+  that we can go to that page and searching that word and easily find the word.
+  First We Search first Character of our word in the index page so that we get
+  the page number for that character. This save our lot of time and work, similarly
+  we can do this type of indexing in Our database so that if Any Query User Raised
+  We can Search the Document effifciently in Our Database. This save lot of time
+  to search document in the database and response quickly by comparing to search
+  withOut Indexing of database
+  
+  we can createIndex by giving the command :
+
+  db.collection("collection_name").createIndex()
+
+  We Can Create the Index by accending Order or Decsending Order we can also create
+  the text indexing means we can give indexing in the text format.
+
+  By Adding the Indexing Our read Operations becomes faster But every time when we
+  insert the document we need to perform extra work by creating the index for the
+  document so we can say our write operations become comparatively slower then before
+*/
