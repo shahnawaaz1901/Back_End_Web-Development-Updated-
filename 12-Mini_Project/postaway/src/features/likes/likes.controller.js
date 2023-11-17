c2830e9ffd3462c1d47a4eab0cb135c66e4607c0
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-11-17 20:55:09 +0530

    Complete the Like Feature

--- 12-Mini_Project/postaway/src/features/likes/likes.controller.js
+++ 12-Mini_Project/postaway/src/features/likes/likes.controller.js
@@ -14,9 +14,7 @@ export default class LikesController {
   removeLikeToPost(req, res) {
     const { postId } = req.params;
     const userId = req.userId;
-    console.log(postId, userId);
     const result = LikesModel.remove(postId, userId);
-    console.log(result, "After the Remove Function Called !!");
     if (!result) {
       return res.status(404).send("Post Not Found !!");
     }
--- 12-Mini_Project/postaway/src/features/likes/likes.model.js
+++ 12-Mini_Project/postaway/src/features/likes/likes.model.js
@@ -18,35 +18,25 @@ export default class LikesModel {
   }
 
   static remove(postId, userId) {
-    console.log("Inside the Removen function")
     let allPosts = PostsModel.getAll(userId);
     const postIndex = allPosts.findIndex(
       (f) => postId == f.id && userId == f.userId
     );
-    console.log("Before postIndex Condition");
     if (postIndex == -1) {
       console.log("Inside the postIndex");
       return;
     }
-    console.log("After postIndex Condition");
     // console.log(allPosts[postIndex]);
     if (!allPosts[postIndex].likes) {
-      allPosts[postIndex].likes = [];
-    }
-    console.log("After Likes Array Creation");
-    const userIndex = allPosts[postIndex].likes.findIndex((u) => {
-      u.userId == userId;
-    });
-    console.log("After the Iterate the array " + userIndex);
-    for (let every of allPosts[postIndex].likes) {
-      console.log(every, "In Loop");
-    }
-
-    if (userIndex == -1) {
       return;
     }
 
-    allPosts[postIndex].likes.splice(userIndex, 1);
+    const likeArray = allPosts[postIndex].likes;
+    const userIndex = likeArray.findIndex((l) => l.userId == userId);
+    if (userIndex == -1) {
+      return;
+    }
+    likeArray.splice(userIndex, 1);
     return allPosts[postIndex];
   }
 }
