29560721cead3143c97d94e8dc118e06ab2733a1
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-11-18 19:14:34 +0530

    UnderStand Usage Of Projection Operators

---
+++ 15-MongoDB_with_Node.js_II/05-Projection_Operators.js
@@ -0,0 +1,9 @@
+/*
+    Projection Operators : When We Search a Document by giving the Condition in
+                Database we Get the Whole Document which is Stored in Database
+                Sometime We Store Additional Information in Database Which We
+                don't want to send. For this We Need to modify the document which
+                satisfy the condition which is given by the user. So Modfying the
+                Document Which we Receive from the Database Some Special kind of
+                Operators mongoDb gives us, Which is Called Projection Operators
+*/
