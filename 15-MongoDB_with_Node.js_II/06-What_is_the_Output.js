01bff5030ad67187a85483f7a8ace57e93684280
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-11-20 18:43:31 +0530

    MCQ Around Transactions

---
+++ 15-MongoDB_with_Node.js_II/06-What_is_the_Output.js
@@ -0,0 +1,28 @@
+/*
+Data Consistency Assurance
+
+What is the primary purpose of using transactions in a database .?
+
+1. Enhancing query performance
+2. Ensuring data consistency and integrity                      //* Correct
+3. Reducing network latency
+4. Simplifying database schema
+
+Solution Description : Transactions are used to group multiple operations into an 
+                atomic unit, ensuring that either all the operations succeed or none 
+                of them do. This guaranteed data consistency and integrity.
+*/
+
+/*
+Processing Fianancial Transactions
+
+Which of the following scenario is best suited for using transactions .?
+
+1. Logging user interactions on a website
+2. Storing cached data for faster retrieval
+3. Processing financial transactions between bank accounts                  //* Correct
+4. Collecting user preferences for a recommendation system
+
+Solution Description : Transactions are critical for maintaining data integrity 
+                in fianancial transactions.
+*/
