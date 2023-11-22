42e40253831f26cf7487f43933c09cd8aa496b0c
shaan.ansari1901@gmail.com <shaan.ansari1901@gmail.com>
2023-11-22 21:48:21 +0530

    MCQ Around mongoose

---
+++ 16-Working_with_Mongoose_I/01-What_is_the_Output.js
@@ -0,0 +1,21 @@
+/* 
+Mongoose Advatages 
+
+What are some key advantages of using Mongoose as an Object-Data modeling 
+(ODM) library for MongoDB in a Node.js application (Multiple)
+
+1. Mongoose allows you to define a schema for your data, providing a clear      //* Correct
+    structure and validation for your documents.
+2. Mongoose is designed explicitly for SQL databases, making it more suitable 
+    than other ODM libraries.
+3. Mongoose provides an easy-to-use API for performing queries on MongoDB,      //* Correct
+    making retrieving, updating, and deleting documents more straightforward.
+4. Mongoose offers middleware hooks to execute functions before or after        //* Correct
+    specific database operations, enabling custom logic and data manipulation.
+
+Solution Description : The Advantages of Mongoose are schema definition, 
+            middleware support, and simplified querying, with one incorrect 
+            option mentaining optimization for SQL databases
+
+
+*/
