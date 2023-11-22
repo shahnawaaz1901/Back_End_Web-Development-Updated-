40cd98e3b1e22c38ee84dfe5d0d574bf7bde098a
shaan.ansari1901@gmail.com <shaan.ansari1901@gmail.com>
2023-11-22 21:43:35 +0530

    UnderStand mongoose ODM

---
+++ 16-Working_with_Mongoose_I/00-Understanding_Mongoose.js
@@ -0,0 +1,36 @@
+/*
+    We can add More Features in Our E-Com Application But While Adding the
+    New Feature we need More Better Tools because for adding every feature
+    we do more number of calls to the databases do more transactions to 
+    database. So we need more better tools to Enhance Our Application Such
+    as Like "Mongoose". Mongoose is an "ODM" tool. ODM Stands for "Object 
+    Document Mapping Tool" . Another Similar word is ORM "Object Relational 
+    Mapping", But ORM Tools Use for Relational Databases. Because we use
+    NoSQL Databases we have ODM such as Mongoose. Mongoose is Most Popular
+    ODM Tool for MongoDB.
+
+*/
+
+/*
+    Benefits of Mongoose :
+    1. ODM : Purpose of ODM is to Provide Better and Easy Interaction to the
+        Database. Currently We Use the Default node.js package of mongodb but
+        when we use mongoose it provides better transactions calls in easier
+        format
+
+    2. Better Structure : Mongoose Provides Better Structure Like Schemas. We
+        Know that in NoSQL Databases We Dont need any kind of schemas but in
+        some cases we need schemas for give the Structure the to the Database
+
+    3. Validations : Before Storing the Data in Database we are Not Validate the
+        Data because because we dont have any option but using the mongoose we
+        have middlewares for validation before storing the data 
+
+    4. More Controls using middlewares : Because we have middlewares so that we
+        have more controls 
+
+    5. RICH API : RICH API is provided by Mongoose ODM which enhance the API Calls
+        while working on Real world Major Applications
+
+    //*Mongoose Not Replace the mongodb but enhance interaction with Database
+*/
