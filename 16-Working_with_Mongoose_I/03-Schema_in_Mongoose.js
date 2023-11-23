cf2c37a49224a3dec8f11e2814bc155f6d6c4b08
shaan.ansari1901@gmail.com <shaan.ansari1901@gmail.com>
2023-11-23 18:03:46 +0530

    Introduction to Schema

---
+++ 16-Working_with_Mongoose_I/03-Schema_in_Mongoose.js
@@ -0,0 +1,33 @@
+/*
+    Schema in Mongoose : Everything in mongoose starts with a Schema. Each schema maps to a 
+                MongoDB Collection and defines the shape of Documents within that Collection. 
+                In Simple Word we can say schema is Like a blueprint shape of the data. We 
+                Already use blueprint of Data known as model, schema is very similar with 
+                models. But schema is Not type of class which we use to create models in Our 
+                Node.js Application.
+                We Can say Mongoose Gives a structure to the collection means its define the 
+                shape that how our document Looks like in the collection
+    
+    In the Schema we have schemaType. SchemaTypes nothing but we can say a data type of the 
+    attribute in the document. Attribute is the fields which we define while creating a Schema.
+    
+    SchemaTypes : 
+    1. String
+    2. Number
+    3. Date
+    4. Buffer
+    5. Boolean
+    6. Mixed
+    7. ObjectId
+    8. Array
+    9. Decimal128
+    10. Map
+    11. Schema
+    12. UUID
+    13. Bight
+
+    Schema Not Just gives the structure to the database but Also very powerful tool which 
+    forces fields to gives the actual data and validate the fields and its types. So that 
+    we can validate our document before storing into the database 
+
+*/
