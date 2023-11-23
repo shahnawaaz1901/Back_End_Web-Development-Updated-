55791304f8d577ad8f5af41837da7b4f633834b3
shaan.ansari1901@gmail.com <shaan.ansari1901@gmail.com>
2023-11-23 19:29:07 +0530

    Introduction to Models

---
+++ 16-Working_with_Mongoose_I/04-Models_in_Mongoose.js
@@ -0,0 +1,8 @@
+/*
+    Models : In world of mongoose models and schema is different Types. Schema is
+        the blueprint or structure of the document in the collection where models
+        is the constructor compiled from schema definations. We can understand like
+        models is set of APIs and Instance an Instance of Model is called as Document.
+        Using Models we give the structure to our collection but using models we can
+        perform data related operations CRUD.
+*/
