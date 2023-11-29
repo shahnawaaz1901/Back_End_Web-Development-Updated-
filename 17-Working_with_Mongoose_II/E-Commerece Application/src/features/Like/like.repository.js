260c4e28df26168edee56ddb27a4f3750c7e5a99
shaan.ansari1901@gmail.com <shaan.ansari1901@gmail.com>
2023-11-29 21:24:44 +0530

    Start to Implement Complex Relations

---
+++ 17-Working_with_Mongoose_II/E-Commerece Application/src/features/Like/like.repository.js
@@ -0,0 +1,38 @@
+import mongoose, { mongo } from "mongoose";
+import { likeSchema } from "./like.schema.js";
+import { ObjectId } from "mongodb";
+import ApplicationError from "../errorHandler/application.error";
+
+const LikeModel = mongoose.model("likes", likeSchema);
+
+export default class LikeRepository {
+  // Like the Product
+  async likeProducts(productId, userId) {
+    try {
+      const newLike = new LikeModel({
+        user: new ObjectId(userId),
+        likeable: new ObjectId(productId),
+        on_model: "Products",
+      });
+      await newLike.save();
+    } catch (error) {
+      console.log(error);
+      throw new ApplicationError("Something Went Wrong with Database", 500);
+    }
+  }
+
+  // Like the Category
+  async likeCategory(categoryId, userId) {
+    try {
+      const newLike = new LikeModel({
+        user: new ObjectId(userId),
+        likeable: new ObjectId(categoryId),
+        on_model: "Category",
+      });
+      await newLike.save();
+    } catch (error) {
+      console.log(error);
+      throw new ApplicationError("Something Went Wrong with Database", 500);
+    }
+  }
+}
---
+++ 17-Working_with_Mongoose_II/E-Commerece Application/src/features/Like/like.schema.js
@@ -0,0 +1,33 @@
+
+import mongoose, { mongo } from "mongoose";
+
+export const likeSchema = new mongoose.Schema({
+    /* Storing useId which like the Product or Category */
+    user : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "users",
+        required : true,
+    },
+    /* 
+        We can't Directly Specify the Product and Category attribute because 
+        if user Like the Product then we need to reffer the to the product
+        collection or if user like the category then we need to reffer to 
+        the category collection so we use word likeable which can't specify
+        a product or category. In likeable we store the type which is id because
+        product and category both have id. because we dont know user like the 
+        product or category so that so we cant directly reffer to a specific
+        collection so we need a keyword which we use to define another attribute
+        which is enum which decide in which collection we want to store the document
+        and the collection name is string so that we specify the type which is string
+    */
+    likeable : {
+        type : mongoose.Schema.Types.ObjectId,
+        required : true,
+        refPath : "on_model"
+    },
+    on_model : {
+        type : String,
+        enum : ["Products","Category"]
+    },
+
+})
