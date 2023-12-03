98b50a44981273e5c6329a9b87e159f30d2aaff7
shaan.ansari1901@gmail.com <shaan.ansari1901@gmail.com>
2023-12-03 10:15:38 +0530

    Added Comments

--- 12-Mini_Project/postaway/src/features/comments/comments.router.js
+++ 12-Mini_Project/postaway/src/features/comments/comments.router.js
@@ -4,9 +4,9 @@ import CommentsController from "./comments.controller.js";
 const commentsRouter = express.Router();
 const commentsController = new CommentsController();
 
-commentsRouter.get("/:postId", commentsController.get);
-commentsRouter.post("/", commentsController.add);
-commentsRouter.put("/:postId", commentsController.update);
-commentsRouter.delete("/:commentId", commentsController.delete);
+commentsRouter.get("/:postId", commentsController.get); //* Get Comments of a Post
+commentsRouter.post("/", commentsController.add); //* Create Comment on Post
+commentsRouter.put("/:postId", commentsController.update); //* Update Comment on Post
+commentsRouter.delete("/:commentId", commentsController.delete); //* Delete Comment of Post
 
 export default commentsRouter;
--- 12-Mini_Project/postaway/src/features/likes/likes.router.js
+++ 12-Mini_Project/postaway/src/features/likes/likes.router.js
@@ -4,8 +4,8 @@ import LikesController from "./likes.controller.js";
 const likesRouter = express.Router();
 const likesController = new LikesController();
 
-likesRouter.post("/addLike/:postId", likesController.addLikeToPost);
-likesRouter.post("/removeLike/:postId", likesController.removeLikeToPost);
-likesRouter.get("/",likesController.getPostLikes);
+likesRouter.get("/", likesController.getPostLikes); //* Get all Likes of a Post
+likesRouter.post("/addLike/:postId", likesController.addLikeToPost); //* Add Like to Post
+likesRouter.post("/removeLike/:postId", likesController.removeLikeToPost); //* Remove Like of Post
 
 export default likesRouter;
