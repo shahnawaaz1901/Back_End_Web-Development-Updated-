0464665f5765ee74923e0379816e430c280f4e7e
shaan.ansari1901@gmail.com <shaan.ansari1901@gmail.com>
2023-12-04 22:14:34 +0530

    MCQ Around Socket.io

---
+++ 18-Socket_Programming/02-What_is_the_Output.js
@@ -0,0 +1,17 @@
+/*
+Socket.io Advantages
+
+What are some advantages of using Socket.io over traditional HTTP requests for 
+real-time communication .?( Multiple )
+
+1. Lower latency for real-time updates                          //* Correct
+2. Scalability for handling a large number of clients           //* Correct
+3. Increased complexity of the program
+4. Efficient bidirectional communication                        //* Correct
+
+Solution Description : The option "Increased complexity of the program" is not 
+            typically considered an advantage of using Socket.io but can something 
+            be a drawback, depending on the specific use case and the complexity of 
+            the implementation.
+
+*/
