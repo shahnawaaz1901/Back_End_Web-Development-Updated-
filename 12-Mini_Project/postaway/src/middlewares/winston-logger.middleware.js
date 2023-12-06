0a5d2c4d23050e44b123bdf8a3ff3e0f8430bd28
shaan.ansari1901@gmail.com <shaan.ansari1901@gmail.com>
2023-12-06 22:14:21 +0530

    Fixing Some Bug in Log middleware

--- 12-Mini_Project/postaway/src/middlewares/winston-logger.middleware.js
+++ 12-Mini_Project/postaway/src/middlewares/winston-logger.middleware.js
@@ -3,19 +3,15 @@ import winston from "winston";
 const createlogger = winston.createLogger({
   level: "info",
   format: winston.format.json(),
-  defaultMeta: "request-logger",
-  transports : [
-    new winston.transports.File({filename : "error.log", level : "error"}),
-    new winston.transports.File({filename : "combined.log"})
-  ]
+  defaultMeta: { service: "request-logging" },
+  transports: [new winston.transports.File({ filename: "error.log" })],
 });
 
 export const logger = (req, res, next) => {
   if (!req.url.includes("users")) {
-    next();
+    const logData = `${req.url} ${JSON.stringify(req.body)}`;
+    createlogger.info(logData);
   }
 
-  const logData = `${req.url}${JSON.stringify(req.body)}`;
-  createlogger.info(logData);
   next();
 };
