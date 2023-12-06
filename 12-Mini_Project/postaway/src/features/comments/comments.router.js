c2142a65a44ae7cdadfffa2b5ff47a6d7a227a16
shaan.ansari1901@gmail.com <shaan.ansari1901@gmail.com>
2023-12-06 12:50:31 +0530

    Completed the Comments Documentation

--- 12-Mini_Project/postaway/src/features/comments/comments.router.js
+++ 12-Mini_Project/postaway/src/features/comments/comments.router.js
@@ -7,6 +7,6 @@ const commentsController = new CommentsController();
 commentsRouter.get("/get/:postId", commentsController.get); //* Get Comments of a Post
 commentsRouter.post("/create", commentsController.add); //* Create Comment on Post
 commentsRouter.put("/update/:postId", commentsController.update); //* Update Comment on Post
-commentsRouter.delete("/delete/:commentId", commentsController.delete); //* Delete Comment of Post
+commentsRouter.delete("/delete", commentsController.delete); //* Delete Comment of Post
 
 export default commentsRouter;
