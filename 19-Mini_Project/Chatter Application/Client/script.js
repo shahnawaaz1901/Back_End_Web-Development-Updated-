1d69e60fdf81dcde22d17e2f42e0835855317726
shaan.ansari1901@gmail.com <shaan.ansari1901@gmail.com>
2023-12-10 17:50:03 +0530

    Created the Front End for User

---
+++ 19-Mini_Project/Chatter Application/Client/script.js
@@ -0,0 +1,4 @@
+const socket = io.connect("http://localhost:3200");
+const userName = prompt("Please Enter Your Name .?");
+const userProfileName = document.getElementById("user-name");
+userProfileName.innerText = userName;
