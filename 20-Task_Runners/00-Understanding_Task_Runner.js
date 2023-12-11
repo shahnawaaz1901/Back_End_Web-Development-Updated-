cf07a0c9827cb46c430d6a574e6f0effaf202749
shaan.ansari1901@gmail.com <shaan.ansari1901@gmail.com>
2023-12-11 19:00:42 +0530

    Understanding Task Runners

---
+++ 20-Task_Runners/00-Understanding_Task_Runner.js
@@ -0,0 +1,41 @@
+/*
+    Whenever We Are Creating web applications, other then writing program
+    we need to perform certain operations which we need to perform which
+    is : 
+
+    1. Minifying CSS : When Ever we work on major project sometimes we created 
+            multiple CSS file like 100s of CSS files. This ammount of CSS file
+            can affect to Performance of the server and as well as browser where
+            we load the our web application. So For Low latency and high performance
+            we need to compress the CSS files by removing spaces in CSS and 
+            overlape the Common Classes and Other Things.
+        
+    2. Convert SASS to CSS format : SASS(Syntatically Awsome Style Sheet) 
+            converts into the CSS because SASS is the another form CSS to 
+            style the WebPage. This Helps us to Load All SASS files into 
+            webpage at the same time when CSS is loaded
+
+    3. Compressing Images : When Ever We Uploaded some image or file on social 
+            media Application Size of the File is Reduced because low size file 
+            can load quickly and it is easy to handle by the server compare to 
+            actual size of file which ammount of size is anything 
+
+    4. Running test Cases : Run the test cases on the Application to findOut the
+            bugs because in some cases one user is faced bugs but another user
+            not faces that bug so its a developer responsibility to run their
+            application on every device or smartphone.
+
+    5. Deploying Applications : Deployement is the part where we are going to 
+            live our web applications, because we want that our application not 
+            limited in our Computer our application can be accessed from anywhere 
+            and every where because we also cant run our application if our system 
+            is off.
+
+    Above All task is Done by the task Runner which is tool to handle this things
+*/
+
+/*
+    For Task Runners we need to Do some work which is :
+    1. Configurations
+    2. Libraries and Plugins
+*/
