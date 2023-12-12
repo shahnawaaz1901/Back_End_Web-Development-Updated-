da8e73c2b49a0ebc6c8504556b1fa27d9ee03afa
shaan.ansari1901@gmail.com <shaan.ansari1901@gmail.com>
2023-12-12 18:43:14 +0530

    Understand Grunt Task Runner

---
+++ 20-Task_Runners/01-GRUNT_Task_Runner_tool.js
@@ -0,0 +1,24 @@
+/*
+    Why Task Runners : Because for every major application we need to 
+        perform same task like minifying CSS and other all. All we can
+        do manually but its very painful to write same thing again and
+        again so it takes lot of times, so instead of doing all work
+        manually we use task runners which can handle all of this work
+        automatically by its plugins.  
+
+    GRUNT : GRUNT is JavaScript Task Runner. In One word We can specify
+        Task Runner as Automation of things. Where Less work you have to
+        do when performing Repitative task such as minification of css, 
+        image compression , compilation and unit testing of the task
+
+    Why GRUNT : The Grunt ecosystem is huge and its growing everyday. With
+        Literally hundreds of plugins to choose from, you can use Grunt to
+        automate just about anything with minimum of efforts
+
+    Steps for Use Grunt In Projects : 
+
+    1. Install the Grunt using npm Command
+    2. Install Grunt Library
+    3. Final step to create a GRUNT file which contains details of every task
+        and configure the tasks
+*/
