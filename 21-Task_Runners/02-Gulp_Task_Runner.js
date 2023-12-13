5ee5c98c46729aca371e3deae1141241c96e7098
shaan.ansari1901@gmail.com <shaan.ansari1901@gmail.com>
2023-12-13 18:34:06 +0530

    Understand Gulp and Start to Implement

---
+++ 21-Task_Runners/02-Gulp_Task_Runner.js
@@ -0,0 +1,16 @@
+/*
+    Gulp : Gulp is another pipeline which have multiple plugins. Gulp is a 
+        toolkit to automate & enhance workflow Leverage gulp and the flexiblity 
+        of javascript to auomate slow repitative workflows and compose them 
+        into efficient build pipelines.
+
+    Mainly Work of Gulp is on the javascript to minifying the javascript code
+    but this is not mean we can only just compress or reduce the javascript 
+    code but we can also do same thing which we can do using the grunt, but
+    grunt is another task runner which provide us plugins to automate the work
+    same as grunt.
+
+    Here we use gulp for comressing the size of images and optimize the size of
+    images
+
+*/  
