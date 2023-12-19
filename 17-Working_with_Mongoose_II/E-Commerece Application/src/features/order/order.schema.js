21ab279904a647154d9a017d3806d49b8b87a058
shaan.ansari1901@gmail.com <shaan.ansari1901@gmail.com>
2023-12-19 15:38:35 +0530

    Create Order Schema

---
+++ 17-Working_with_Mongoose_II/E-Commerece Application/src/features/order/order.schema.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  time: {
+    type: String,
+    required: true,
+  },
+});
