155b1f9004dab9d46d37236114bb5758e9e123bb
shaan.ansari1901@gmail.com <shaan.ansari1901@gmail.com>
2023-12-22 01:27:40 +0530

    Start to Implement Likes feature

--- 18-Mini_Project/Postaway_II/src/features/likes/likes.controller.js
+++ 18-Mini_Project/Postaway_II/src/features/likes/likes.controller.js
@@ -6,6 +6,17 @@ export default class LikeController {
   }
 
   async addLike(req, res) {
+    const { id, type } = req.body;
+    if (!id || !type) {
+      return res
+        .status(406)
+        .json({ success: false, message: "Please Provide Required details" });
+    }
+    if (type != "Like" && type != "User" && type != "Comment") {
+      return res
+        .status(406)
+        .json({ success: false, message: "Type is Invalid" });
+    }
     const likeData = await this.likeRepository.add(req.body);
   }
 
