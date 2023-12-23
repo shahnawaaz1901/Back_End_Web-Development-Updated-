4dce44d6c602763f2eb5868990e0597152179831
shaan.ansari1901@gmail.com <shaan.ansari1901@gmail.com>
2023-12-23 13:01:22 +0530

    Successfully Implement Like  Feature for Post, User & Comments

--- 18-Mini_Project/Postaway_II/src/features/likes/likes.repository.js
+++ 18-Mini_Project/Postaway_II/src/features/likes/likes.repository.js
@@ -1,13 +1,30 @@
 import LikeModel from "./likes.schema.js";
+import CommentModel from "../comments/comments.schema.js";
+import PostModel from "../posts/posts.schema.js";
+import UserModel from "../users/users.schema.js";
+
 export default class LikeRepository {
   async add(likeInfo) {
-    const like = new LikeModel({
+    const likeData = new LikeModel({
       user: likeInfo.userId,
       likeable: likeInfo.id,
       on_model: likeInfo.type,
     });
-    await like.save();
+    await likeData.save();
+    const entityModel =
+      likeInfo.type == "Post"
+        ? PostModel
+        : likeInfo.type == "Comment"
+        ? CommentModel
+        : UserModel;
+    const entityExist = await entityModel.findById(likeInfo.id);
+    if (!entityExist) {
+      throw new Error(`${id} not found !!`);
+    }
+    entityExist.likes.push(likeData._id);
+    await entityExist.save();
+    return likeData;
   }
 
-  async remove() {}
+  async remove(info) {}
 }
