f1c251afe1131a5f8c15f9384cf48e0faa760df3
shaan.ansari1901@gmail.com <shaan.ansari1901@gmail.com>
2023-12-25 11:36:52 +0530

    Added error.log which log only error specific data

--- 18-Mini_Project/Postaway_II/src/features/error/error.response.js
+++ 18-Mini_Project/Postaway_II/src/features/error/error.response.js
@@ -1,5 +1,7 @@
 import ApplicationError from "./error.class.js";
+import { logger } from "../../middlewares/winston.logger.js";
 const errorMiddleware = (err, req, res, next) => {
+  logger.error({ url: req.url, data: req.body, time: new Date().toString() });
   if (err instanceof ApplicationError) {
     return res
       .status(err.errStatusCode)
