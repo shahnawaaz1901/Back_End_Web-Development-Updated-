fc1ee12fa41c8da73731598a1849c09ab4d44c3b
shaan.ansari1901@gmail.com <shaan.ansari1901@gmail.com>
2024-01-22 12:34:32 +0530

    Create Chats Storing Functionalities to Store and Retrieve Chats

---
+++ 20-Mini_Project/Chatter Application/feature/chats/chats.schema.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const chatSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+  timeStamp: {
+    type: String,
+    required: true,
+  },
+});
+const ChatModel = mongoose.model("Chat", chatSchema);
+export default ChatModel;
--- 20-Mini_Project/Chatter Application/mongoose.js
+++ 20-Mini_Project/Chatter Application/mongoose.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToDB = async () => {
   try {
-    await mongoose.connect(`${process.env.DB_URL}/ChatApp`);
+    await mongoose.connect(`${process.env.DB_URL}/ChatDB`);
     console.log("Database is Connected Via Mongoose");
   } catch (error) {
     console.log("Error while Connecting with Database");
