02fa62140b6384403da04dc69edbe1b7f9aa6a79
shaan.ansari1901@gmail.com <shaan.ansari1901@gmail.com>
2024-04-23 21:26:25 +0530

    Make Some Changes

--- 07-Mini_Project/Job_Portal/src/middlewares/file-upload.middleware.js
+++ 07-Mini_Project/Job_Portal/src/middlewares/file-upload.middleware.js
@@ -4,7 +4,7 @@ import path from "path";
 const diskConfig = multer.diskStorage({
   destination: (req, file, cb) => {
     console.log(path.resolve());
-    cb(null, path.resolve("public"));
+    cb(null, path.resolve("public", "data"));
   },
 
   filename: (req, file, cb) => {
